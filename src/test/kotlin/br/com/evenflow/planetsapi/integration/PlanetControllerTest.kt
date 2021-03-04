f8fcfe3fc9b4e97d4ac23dca45c65a63ebfa4acc
Bruno Marins <nobruin@gmail.com>
2021-03-04 14:49:28 -0300

    coloca novamente a configuração do mysql

--- src/test/kotlin/br/com/evenflow/planetsapi/integration/PlanetControllerTest.kt
+++ src/test/kotlin/br/com/evenflow/planetsapi/integration/PlanetControllerTest.kt
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+@ActiveProfiles("alternative")
 class PlanetControllerTest {
 
     @Autowired lateinit var mockMvc: MockMvc
